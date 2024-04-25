95d7f19ad00d91f914a83b96d3cdaf1e471ce525
KarinaMikaelson <mikael.kar7@gmail.com>
2024-04-24 17:51:18 -0700

    e182-e209 solved

--- src/main/java/org/example/e208/E208Map.java
+++ src/main/java/org/example/e208/E208Map.java
@@ -1,4 +1,4 @@
-package e208;
+package org.example.e208;
 /*
 HashMap, Methods, reusability, Logic
 Create a Method in Main class using below specification.
@@ -26,9 +26,27 @@ map is empty
  */
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class E208Map {
+    public static void display(Map<String,Integer> map){
+        if(map.isEmpty()){
+            System.out.println("map is empty");
+        }else {
+            for(Map.Entry<String,Integer> entry: map.entrySet() ){
+                System.out.println(entry.getKey()+" : "+entry.getValue());
+            }
+        }
+    }
     public static void main(String[] args) {
 
+        Map<String,Integer> map = new HashMap<>();
+        map.put("mango", 10);
+        map.put("apple", 30);
+        map.put("orange", 20);
+        display(map);
+        map.clear();
+        display(map);
     }
-
 }
